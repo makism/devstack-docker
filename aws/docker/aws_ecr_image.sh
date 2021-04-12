0727b1b84d9ef86972b3d92304fa18fd17df6587
Avraam Marimpis <makhsm@gmail.com>
2021-04-12 14:35:17 +0200

    * Add [WIP] instructions to push a local Docker image to Amazon ECR.

---
+++ aws/docker/aws_ecr_image.sh
@@ -0,0 +1,23 @@
+#/bin/env bash
+
+# AWS region; according to your geographic location.
+AWS_REGION=eu-central-1
+# You may find this in your "IAM dashboard".
+AWS_ACCOUNT_ID=
+
+REPO_NAME=fast_api_app
+# The Docker image name must match the name defined in the file `build.sh`
+DOCKER_IMAGE=fastapi:latest
+
+# Login
+# The password will be stored at: ~/.docker/config.json
+/opt/bin/aws ecr get-login-password  --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+# Create repository
+aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION
+
+# Tag your image so you can push the image to this repository
+docker tag $DOCKER_IMAGE $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
+
+# Push the docker image to AWS repository
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
